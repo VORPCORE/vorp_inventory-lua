72d727bc2ef21c4eb42f31ffe0e4ecad29277ade
Luis Gomes <creativewild@users.noreply.github.com>
2022-03-31 19:22:12 +0100

    Updated a mistyped reference
    changed a mistyped reference

--- server/controllers/inventoryController.lua
+++ server/controllers/inventoryController.lua
@@ -1,53 +1,53 @@
 RegisterServerEvent("vorpinventory:getItemsTable")
-AddEventHandler("vorpinventory:getItemsTable", VORPInvetory.getItemsTable)
+AddEventHandler("vorpinventory:getItemsTable", VORPInventory.getItemsTable)
 
 RegisterServerEvent("vorpinventory:getInventory")
-AddEventHandler("vorpinventory:getInventory", VORPInvetory.getInventory)
+AddEventHandler("vorpinventory:getInventory", VORPInventory.getInventory)
 
 RegisterServerEvent("vorpinventory:serverGiveItem")
-AddEventHandler("vorpinventory:serverGiveItem", VORPInvetory.GiveItem)
+AddEventHandler("vorpinventory:serverGiveItem", VORPInventory.GiveItem)
 
 RegisterServerEvent("vorpinventory:serverGiveWeapon")
-AddEventHandler("vorpinventory:serverGiveWeapon", VORPInvetory.GiveWeapon)
+AddEventHandler("vorpinventory:serverGiveWeapon", VORPInventory.GiveWeapon)
 
 RegisterServerEvent("vorpinventory:serverDropItem")
-AddEventHandler("vorpinventory:serverDropItem", VORPInvetory.DropItem)
+AddEventHandler("vorpinventory:serverDropItem", VORPInventory.DropItem)
 
 RegisterServerEvent("vorpinventory:serverDropMoney")
-AddEventHandler("vorpinventory:serverDropMoney", VORPInvetory.DropMoney)
+AddEventHandler("vorpinventory:serverDropMoney", VORPInventory.DropMoney)
 
 RegisterServerEvent("vorpinventory:serverDropAllMoney")
-AddEventHandler("vorpinventory:serverDropAllMoney", VORPInvetory.dropAllMoney)
+AddEventHandler("vorpinventory:serverDropAllMoney", VORPInventory.dropAllMoney)
 
 RegisterServerEvent("vorpinventory:serverDropWeapon")
-AddEventHandler("vorpinventory:serverDropWeapon", VORPInvetory.DropWeapon)
+AddEventHandler("vorpinventory:serverDropWeapon", VORPInventory.DropWeapon)
 
 RegisterServerEvent("vorpinventory:sharePickupServer")
-AddEventHandler("vorpinventory:sharePickupServer", VORPInvetory.sharePickupServer)
+AddEventHandler("vorpinventory:sharePickupServer", VORPInventory.sharePickupServer)
 
 RegisterServerEvent("vorpinventory:shareMoneyPickupServer")
-AddEventHandler("vorpinventory:shareMoneyPickupServer", VORPInvetory.sharePickupServer)
+AddEventHandler("vorpinventory:shareMoneyPickupServer", VORPInventory.sharePickupServer)
 
 RegisterServerEvent("vorpinventory:onPickup")
-AddEventHandler("vorpinventory:onPickup", VORPInvetory.onPickup)
+AddEventHandler("vorpinventory:onPickup", VORPInventory.onPickup)
 
 RegisterServerEvent("vorpinventory:onPickupMoney")
-AddEventHandler("vorpinventory:onPickupMoney", VORPInvetory.onPickupMoney)
+AddEventHandler("vorpinventory:onPickupMoney", VORPInventory.onPickupMoney)
 
 RegisterServerEvent("vorpinventory:setUsedWeapon")
-AddEventHandler("vorpinventory:setUsedWeapon", VORPInvetory.usedWeapon)
+AddEventHandler("vorpinventory:setUsedWeapon", VORPInventory.usedWeapon)
 
 RegisterServerEvent("vorpinventory:setWeaponBullets")
-AddEventHandler("vorpinventory:setWeaponBullets", VORPInvetory.setWeaponBullets)
+AddEventHandler("vorpinventory:setWeaponBullets", VORPInventory.setWeaponBullets)
 
 RegisterServerEvent("vorpinventory:giveMoneyToPlayer")
-AddEventHandler("vorpinventory:giveMoneyToPlayer", VORPInvetory.giveMoneyToPlayer)
+AddEventHandler("vorpinventory:giveMoneyToPlayer", VORPInventory.giveMoneyToPlayer)
 
 --------------------------------- NEW AND UPDATED ----------------------------------------
 
 RegisterServerEvent("vorpinventory:getLabelFromId")
-AddEventHandler("vorpinventory:getLabelFromId", VORPInvetory.getLabelFromId)     
+AddEventHandler("vorpinventory:getLabelFromId", VORPInventory.getLabelFromId)
 
 
 RegisterServerEvent("vorpinventory:check_slots")
-AddEventHandler("vorpinventory:check_slots", VORPInvetory.getSlots) 
+AddEventHandler("vorpinventory:check_slots", VORPInventory.getSlots)
