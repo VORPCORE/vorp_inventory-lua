62b7afef5317d0363d381cbc7fb98b1fb50c713c
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-17 01:30:51 +0100

    PT language

---
+++ languages/pt_lang.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------------------------------------------
+---------------------------------- PORTUGUES -------------------------------------------
+--VORP inventory lua notificacao
+
+Locales["pt_lang"] = {
+
+    fullInventory     = "Muito peso nao podes dar mais coisas",
+    fullInventoryGive = "A pessoa leva muito peso nao podes dar mais coisas",
+    TryExploits       = "nao tentes ser mais esperto que o systema",
+    TakeFromFloor     = "Apanhar",
+    NotEnoughMoney    = "Nao tens assim tanto diheiro",
+    YouPaid           = "Pagaste {0}$ a {1}",
+    YouReceived       = "Recebeste {0}$ de {1}",
+    yougaveitem       = "Deste uma coisa ",
+    itemerror         = "Invalido "
+}
