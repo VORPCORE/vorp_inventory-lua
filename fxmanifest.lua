530cce4f55896b305eead6e4841d1e52f70c94af
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-07 15:01:19 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -30,3 +30,8 @@ files {
 }
 
 server_exports { 'vorp_inventoryApi' }
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/outsider31000/vorp_inventory-lua'
