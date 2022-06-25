2a7afdebb55d31291c02b3ae1ccb5548a60506c3
robizona <robizona.info@gmail.com>
2022-06-25 19:42:37 +0200

    little fix

--- config.lua
+++ config.lua
@@ -9,12 +9,9 @@ Config  = {
   
   defaultlang = "en_lang",
   
-    -- DESCRIPTION WEAPONS 
-    -- DescWeapons = "WEAPONS DESC IN CONFIG", 
-
-    -- GOLD ITEM LIKE DOLLARS
-    UseGoldItem = false, -- IF TRUE YOU HAVE GOLD IN INVENTORY LIKE DOLLARS
-    -- CHANGE IN html/js/config.js TOO !!!
+  -- GOLD ITEM LIKE DOLLARS
+  UseGoldItem = false, -- IF TRUE YOU HAVE GOLD IN INVENTORY LIKE DOLLARS
+  -- CHANGE IN html/js/config.js TOO !!!
   
   -- DEATH FUNCTIONS
   DisableDeathInventory = true, -- prevent the ability to access inventory while dead
