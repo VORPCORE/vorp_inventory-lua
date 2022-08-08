44e82dd936b18bd05b1a3148f699e8d0057d02ff
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-08-08 18:49:49 +0100

    fix for getuserweapons

--- server/services/inventoryApiService.lua
+++ server/services/inventoryApiService.lua
@@ -236,26 +236,28 @@ InventoryAPI.getUserWeapon = function(player, cb, weaponId)
 end
 
 InventoryAPI.getUserWeapons = function(player, cb)
-	local _source = player
-	local sourceCharacter = Core.getUser(_source).getUsedCharacter
-	local identifier = sourceCharacter.identifier
-	local charidentifier = sourceCharacter.charIdentifier
-	local userWeapons = UsersWeapons["default"]
+    local _source = player
+    local sourceCharacter = Core.getUser(_source).getUsedCharacter
+    local identifier = sourceCharacter.identifier
+    local charidentifier = sourceCharacter.charIdentifier
+    local usersWeapons = UsersWeapons["default"]
 
-	for _, currentWeapon in pairs(userWeapons) do
-		if currentWeapon:getPropietary() == identifier and currentWeapon:getCharId() == charidentifier then
-			local weapon = {
-				name = currentWeapon:getName(),
-				id = currentWeapon:getId(),
-				propietary = currentWeapon:getPropietary(),
-				used = currentWeapon:getUsed(),
-				ammo = currentWeapon:getAllAmmo(),
-				desc = currentWeapon:getDesc()
-			}
-			table.insert(userWeapons, weapon)
-		end
-	end
-	cb(userWeapons)
+    local userWeapons = {}
+
+    for _, currentWeapon in pairs(usersWeapons ) do
+        if currentWeapon:getPropietary() == identifier and currentWeapon:getCharId() == charidentifier then
+            local weapon = {
+                name = currentWeapon:getName(),
+                id = currentWeapon:getId(),
+                propietary = currentWeapon:getPropietary(),
+                used = currentWeapon:getUsed(),
+                ammo = currentWeapon:getAllAmmo(),
+                desc = currentWeapon:getDesc()
+            }
+            table.insert(userWeapons, weapon)
+        end
+    end
+    cb(userWeapons)
 end
 
 InventoryAPI.getWeaponBullets = function(player, cb, weaponId)
