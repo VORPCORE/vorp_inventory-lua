2790e137693f4ab29cc1825cb9271029bbddde5c
Blue <62485293+kamelzarandah@users.noreply.github.com>
2023-01-14 08:44:31 -0800

    optimisation

--- server/server.lua
+++ server/server.lua
@@ -21,26 +21,13 @@ end
 RegisterServerEvent("vorpinventory:check_slots")
 AddEventHandler("vorpinventory:check_slots", function()
     local _source = tonumber(source)
-    local eq = 0
-    if _source ~= 0 and _source >= 1 and _source ~= nil then
-        eq = VorpInv.getUserInventory(_source)
-    end
-
-    local test = eq
-    local slot_check = 0
-
-    if test ~= nil then
-        for i = 1, #test do
-            slot_check = slot_check + test[i].count
-        end
-    else
-        slot_check = 0
-    end
-    local stufftosend = tonumber(slot_check)
     local part2 = Config.MaxItemsInInventory.Items
     local User = Core.getUser(_source).getUsedCharacter
+    local identifier = User.identifier
     local money = User.money
     local gold = User.gold
+    local stufftosend = InventoryAPI.getUserTotalCount(identifier)
+
     TriggerClientEvent("syn:getnuistuff", _source, stufftosend, part2, money, gold)
 end)
 
