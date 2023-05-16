09ffaaed99839fe1621093a0f04552878dfaefd9
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-05-16 16:33:08 +0100

    fix: for string.format notification

--- server/services/inventoryService.lua
+++ server/services/inventoryService.lua
@@ -188,10 +188,10 @@ InventoryService.giveMoneyToPlayer = function(target, amount)
 		else
 			sourceCharacter.removeCurrency(0, amount)
 			targetCharacter.addCurrency(0, amount)
-			TriggerClientEvent("vorp:TipRight", _source, string.format(T.YouPaid, tostring(amount) .. " ID: " .. _target),
+			TriggerClientEvent("vorp:TipRight", _source, string.format(T.YouPaid, tostring(amount), " ID: " .. _target),
 				3000)
 			TriggerClientEvent("vorp:TipRight", _target,
-				string.format(T.YouReceived, tostring(amount) .. " ID: " .. _source), 3000)
+				string.format(T.YouReceived, tostring(amount), " ID: " .. _source), 3000)
 			Wait(3000)
 			TriggerClientEvent("vorp_inventory:ProcessingReady", _source)
 			local title = T.gaveMoney
@@ -270,9 +270,9 @@ InventoryService.giveGoldToPlayer = function(target, amount)
 		sourceCharacter.removeCurrency(1, amount)
 		targetCharacter.addCurrency(1, amount)
 
-		TriggerClientEvent("vorp:TipRight", _source, string.format(T.YouPaid, tostring(amount) .. "ID: " .. _target),
+		TriggerClientEvent("vorp:TipRight", _source, string.format(T.YouPaid, tostring(amount), "ID: " .. _target),
 			3000)
-		TriggerClientEvent("vorp:TipRight", _target, string.format(T.YouReceived, tostring(amount) .. "ID: " .. _source),
+		TriggerClientEvent("vorp:TipRight", _target, string.format(T.YouReceived, tostring(amount), "ID: " .. _source),
 			3000)
 		TriggerClientEvent("vorp_inventory:ProcessingReady", _source)
 		Wait(3000)
