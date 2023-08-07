ac7882787e53d16d5c29159b8f4a2dfd6dfeb6be
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-08-07 02:24:57 +0100

    version 2.8 update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,19 @@
 game 'rdr3'
 fx_version 'cerulean'
 
-author 'VORP Core'
+author 'VORP'
 description 'VORP Inventory'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 ui_page 'html/ui.html'
 lua54 'yes'
+
+shared_scripts {
+  "config.lua",
+  "languages/*.lua",
+  "shared/models/*.lua",
+  "shared/services/*.lua",
+  "shared/services/Regex.js"
+}
 
 client_scripts {
   'client/models/*.lua',
@@ -14,6 +22,7 @@ client_scripts {
   'client/controllers/*.lua',
   'client/*.lua',
 }
+
 
 server_scripts {
   'server/models/*.lua',
@@ -25,20 +34,14 @@ server_scripts {
   '@oxmysql/lib/MySQL.lua'
 }
 
-shared_scripts {
-  "config.lua",
-  "languages/*.lua",
-  "shared/models/*.lua",
-  "shared/services/*.lua",
-  "shared/services/Regex.js"
-}
 
-files { 'html/**/*' }
+
+files { 'html/**/*' } 
 
 server_exports { 'vorp_inventoryApi' }
 
 
-version '2.7'
+version '2.8'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_inventory-lua'
