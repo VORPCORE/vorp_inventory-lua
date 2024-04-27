9f69f3aae9c2fa6a2f20fdd1a9ad5b2e457fcda1
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-04-27 20:34:39 +0100

    fix:(server/DB): non shared inventories would not remove items from container
    this has been here this whole time
    it was missing the character_id , it would only update when you add items but on restart the items were duplicated
    
    this should fix it

--- server/services/DbService.lua
+++ server/services/DbService.lua
@@ -15,7 +15,7 @@ function DBService.GetSharedInventory(inventoryId, cb)
 end
 
 function DBService.GetInventory(charIdentifier, inventoryId, cb)
-    MySQL.query("SELECT ic.id, i.item, ci.amount, ic.metadata, ci.created_at FROM items_crafted ic\
+    MySQL.query("SELECT ic.id, i.item, ci.amount, ic.metadata, ci.created_at, ci.character_id FROM items_crafted ic\
 		LEFT JOIN character_inventories ci on ic.id = ci.item_crafted_id\
 		LEFT JOIN items i on ic.item_id = i.id\
 		WHERE ci.inventory_type = @invType\
@@ -30,42 +30,38 @@ function DBService.GetInventory(charIdentifier, inventoryId, cb)
 end
 
 function DBService.GiveItem(giverCharIdentifier, receiverCharIdentifier, itemCraftedId)
-    MySQL.update(
-        "UPDATE character_inventories SET character_id = @receiver WHERE character_id = @giver AND item_crafted_id = @itemid;"
-        , {
-            ['receiver'] = tonumber(giverCharIdentifier),
-            ['giver'] = tonumber(receiverCharIdentifier),
-            ['itemid'] = tonumber(itemCraftedId)
-        })
+    MySQL.update("UPDATE character_inventories SET character_id = @receiver WHERE character_id = @giver AND item_crafted_id = @itemid;"
+    , {
+        ['receiver'] = tonumber(giverCharIdentifier),
+        ['giver'] = tonumber(receiverCharIdentifier),
+        ['itemid'] = tonumber(itemCraftedId)
+    })
 end
 
 function DBService.SetItemAmount(sourceCharIdentifier, itemCraftedId, amount)
-    MySQL.update(
-        "UPDATE character_inventories SET amount = @amount WHERE character_id = @charid AND item_crafted_id = @itemid;"
-        , {
-            ['amount'] = tonumber(amount),
-            ['charid'] = tonumber(sourceCharIdentifier),
-            ['itemid'] = tonumber(itemCraftedId)
-        })
+    MySQL.update("UPDATE character_inventories SET amount = @amount WHERE character_id = @charid AND item_crafted_id = @itemid;"
+    , {
+        ['amount'] = tonumber(amount),
+        ['charid'] = tonumber(sourceCharIdentifier),
+        ['itemid'] = tonumber(itemCraftedId)
+    })
 end
 
 function DBService.SetItemMetadata(sourceCharIdentifier, itemCraftedId, metadata)
-    MySQL.update(
-        "UPDATE items_crafted SET metadata = @metadata WHERE character_id = @charid AND id = @itemid;"
-        , {
-            ['metadata'] = json.encode(metadata), -- Check if need to json.encode().
-            ['charid'] = tonumber(sourceCharIdentifier),
-            ['itemid'] = tonumber(itemCraftedId)
-        })
+    MySQL.update("UPDATE items_crafted SET metadata = @metadata WHERE character_id = @charid AND id = @itemid;"
+    , {
+        ['metadata'] = json.encode(metadata),     -- Check if need to json.encode().
+        ['charid'] = tonumber(sourceCharIdentifier),
+        ['itemid'] = tonumber(itemCraftedId)
+    })
 end
 
 function DBService.DropItem(sourceCharIdentifier, itemCraftedId)
-    MySQL.update(
-        "UPDATE character_inventories SET character_id = NULL WHERE character_id = @charid  AND item_crafted_id = @itemid;"
-        , {
-            ['charid'] = tonumber(sourceCharIdentifier),
-            ['itemid'] = tonumber(itemCraftedId)
-        })
+    MySQL.update("UPDATE character_inventories SET character_id = NULL WHERE character_id = @charid  AND item_crafted_id = @itemid;"
+    , {
+        ['charid'] = tonumber(sourceCharIdentifier),
+        ['itemid'] = tonumber(itemCraftedId)
+    })
 end
 
 function DBService.PickupItem(sourceCharIdentifier, itemCraftedId)
@@ -77,52 +73,50 @@ function DBService.PickupItem(sourceCharIdentifier, itemCraftedId)
 end
 
 function DBService.DeleteItem(sourceCharIdentifier, itemCraftedId)
-    MySQL.query(
-        "DELETE FROM character_inventories WHERE character_id = @charid AND item_crafted_id = @itemid;"
-        , {
-            ['charid'] = tonumber(sourceCharIdentifier),
+    MySQL.query("DELETE FROM character_inventories WHERE character_id = @charid AND item_crafted_id = @itemid;"
+    , {
+        ['charid'] = tonumber(sourceCharIdentifier),
+        ['itemid'] = tonumber(itemCraftedId)
+    }, function()
+        MySQL.query("DELETE FROM items_crafted WHERE id = @itemid;", {
             ['itemid'] = tonumber(itemCraftedId)
-        }, function()
-            MySQL.query("DELETE FROM items_crafted WHERE id = @itemid;", {
-                ['itemid'] = tonumber(itemCraftedId)
-            })
-        end)
+        })
+    end)
 end
 
 function DBService.CreateItem(sourceCharIdentifier, itemId, amount, metadata, cb, invId)
     invId = invId or "default"
-    MySQL.insert(
-        "INSERT INTO items_crafted (character_id, item_id, metadata) VALUES (@charid, @itemid, @metadata);"
-        , {
-            ['charid'] = tonumber(sourceCharIdentifier),
-            ['itemid'] = tonumber(itemId),
-            ['metadata'] = json.encode(metadata)
-        }, function()
-            MySQL.query(
-                "SELECT * FROM items_crafted WHERE character_id = @charid AND item_id = @itemid AND JSON_CONTAINS(metadata, @metadata);"
-                , {
-                    ['charid'] = tonumber(sourceCharIdentifier),
-                    ['itemid'] = tonumber(itemId),
-                    ['metadata'] = json.encode(metadata)
-                }, function(result)
-                    if result[1] and result[#result] then
-                        local item = result[#result]
-                        if item then
-                            local itemCraftedId = item.id
-                            MySQL.insert(
-                                "INSERT INTO character_inventories (character_id, item_crafted_id, amount, inventory_type) VALUES (@charid, @itemid, @amount, @invId);"
-                                , {
-                                    ['charid'] = tonumber(sourceCharIdentifier),
-                                    ['itemid'] = tonumber(itemCraftedId),
-                                    ['amount'] = tonumber(amount),
-                                    ['invId'] = invId
-                                }, function()
-                                    cb({ id = itemCraftedId })
-                                end)
-                        end
+    MySQL.insert("INSERT INTO items_crafted (character_id, item_id, metadata) VALUES (@charid, @itemid, @metadata);"
+    , {
+        ['charid'] = tonumber(sourceCharIdentifier),
+        ['itemid'] = tonumber(itemId),
+        ['metadata'] = json.encode(metadata)
+    }, function()
+        MySQL.query(
+            "SELECT * FROM items_crafted WHERE character_id = @charid AND item_id = @itemid AND JSON_CONTAINS(metadata, @metadata);"
+            , {
+                ['charid'] = tonumber(sourceCharIdentifier),
+                ['itemid'] = tonumber(itemId),
+                ['metadata'] = json.encode(metadata)
+            }, function(result)
+                if result[1] and result[#result] then
+                    local item = result[#result]
+                    if item then
+                        local itemCraftedId = item.id
+                        MySQL.insert(
+                            "INSERT INTO character_inventories (character_id, item_crafted_id, amount, inventory_type) VALUES (@charid, @itemid, @amount, @invId);"
+                            , {
+                                ['charid'] = tonumber(sourceCharIdentifier),
+                                ['itemid'] = tonumber(itemCraftedId),
+                                ['amount'] = tonumber(amount),
+                                ['invId'] = invId
+                            }, function()
+                                cb({ id = itemCraftedId })
+                            end)
                     end
-                end)
-        end)
+                end
+            end)
+    end)
 end
 
 ---delete asynchronously
@@ -156,7 +150,6 @@ end
 function DBService.queryAsync(query, params, cb)
     MySQL.query(query, params, cb)
 end
-
 
 --- query asynchronously
 ---@param query string @SQL query
