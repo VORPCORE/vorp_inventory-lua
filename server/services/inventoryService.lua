7ef2444054df14ca826f29049116587a0cac4da7
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-05-21 16:14:47 +0100

    fix: get ammo (typo)

--- server/services/inventoryService.lua
+++ server/services/inventoryService.lua
@@ -1038,7 +1038,7 @@ function InventoryService.getInventory()
 end
 
 Core.Callback.Register("vorpinventory:getammoinfo", function(source, cb)
-	if not allplayersammo[_source] then
+	if not allplayersammo[source] then
 		return cb(false)
 	end
 
