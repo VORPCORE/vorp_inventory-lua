0a5a469604b8b574b604ae98350a71a9131b2eb3
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-11-03 04:24:40 +0000

    chore(server): clean up respawn remove  items event

--- server/respawn.lua
+++ server/respawn.lua
@@ -4,51 +4,51 @@ local Core = exports.vorp_core:GetCore()
 RegisterNetEvent("vorp:PlayerForceRespawn", function()
     local _source = source
 
-    local User = Core.getUser(_source)
-    if not User then return end
+    local user <const> = Core.getUser(_source)
+    if not user then return end
 
-    User = User.getUsedCharacter
-    local job = User.job
-    local isdead = User.isdead
+    local character <const> = user.getUsedCharacter()
+    local job <const> = character.job
+    local isdead <const> = character.isdead
 
     if not Config.UseClearAll or not isdead then
         return
     end
 
-    local _value <const> = Config.OnPlayerRespawn
+    local value <const> = Config.OnPlayerRespawn
 
     --MONEY
-    if _value.Money.ClearMoney then
-        if not SharedUtils.IsValueInArray(job, _value.Money.JobLock) then
-            if not _value.Money.MoneyPercentage then
-                User.removeCurrency(0, User.money)
+    if value.Money.ClearMoney then
+        if not SharedUtils.IsValueInArray(job, value.Money.JobLock) then
+            if not value.Money.MoneyPercentage then
+                user.removeCurrency(0, user.money)
             else
-                User.removeCurrency(0, User.money * _value.Money.MoneyPercentage)
+                user.removeCurrency(0, user.money * value.Money.MoneyPercentage)
             end
         end
     end
 
     -- GOLD
-    if _value.Gold.ClearGold then
-        if not SharedUtils.IsValueInArray(job, _value.Gold.JobLock) then
-            if not _value.Gold.GoldPercentage then
-                User.removeCurrency(1, User.gold)
+    if value.Gold.ClearGold then
+        if not SharedUtils.IsValueInArray(job, value.Gold.JobLock) then
+            if not value.Gold.GoldPercentage then
+                user.removeCurrency(1, user.gold)
             else
-                User.removeCurrency(1, User.gold * _value.Gold.GoldPercentage)
+                user.removeCurrency(1, user.gold * value.Gold.GoldPercentage)
             end
         end
     end
 
     -- ITEMS
     CreateThread(function()
-        if _value.Items.AllItems then
-            if not SharedUtils.IsValueInArray(job, _value.Items.JobLock) then
+        if value.Items.AllItems then
+            if not SharedUtils.IsValueInArray(job, value.Items.JobLock) then
                 InventoryAPI.getInventory(_source, function(Userinventory)
                     for i, item in pairs(Userinventory) do
                         Wait(20)
-                        if next(_value.Items.itemWhiteList) then
-                            for index, value in ipairs(_value.Items.itemWhiteList) do
-                                if item.name ~= value then
+                        if next(value.Items.itemWhiteList) then
+                            for index, v in ipairs(value.Items.itemWhiteList) do
+                                if item.name ~= v then
                                     InventoryAPI.subItem(_source, item.name, item.count, item.metadata)
                                 end
                             end
@@ -63,13 +63,13 @@ RegisterNetEvent("vorp:PlayerForceRespawn", function()
 
     -- WEAPONS
     CreateThread(function()
-        if _value.Weapons.AllWeapons then
-            if not SharedUtils.IsValueInArray(job, _value.Weapons.JobLock) then
+        if value.Weapons.AllWeapons then
+            if not SharedUtils.IsValueInArray(job, value.Weapons.JobLock) then
                 InventoryAPI.getUserWeapons(_source, function(Userweapons)
                     for i, weapon in pairs(Userweapons) do
-                        if next(_value.Weapons.WeaponWhitelisted) then
-                            for index, value in ipairs(_value.Weapons.WeaponWhitelisted) do
-                                if value ~= weapon.name then
+                        if next(value.Weapons.WeaponWhitelisted) then
+                            for index, v in ipairs(value.Weapons.WeaponWhitelisted) do
+                                if v ~= weapon.name then
                                     InventoryAPI.subWeapon(_source, weapon.id)
                                     InventoryAPI.deleteWeapon(_source, weapon.id)
                                 end
@@ -85,8 +85,8 @@ RegisterNetEvent("vorp:PlayerForceRespawn", function()
     end)
 
     --AMMO
-    if _value.Ammo.AllAmmo then
-        if not SharedUtils.IsValueInArray(job, _value.Ammo.JobLock) then
+    if value.Ammo.AllAmmo then
+        if not SharedUtils.IsValueInArray(job, value.Ammo.JobLock) then
             TriggerClientEvent('syn_weapons:removeallammo', _source)  -- syn script
             TriggerClientEvent('vorp_weapons:removeallammo', _source) -- vorp script
             InventoryAPI.removeAllUserAmmo(_source)
