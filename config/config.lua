7545bc71cfcd4a2848cd476017beddb7c13d73bf
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2024-11-27 17:17:48 +0300

    “Config.Filter” has been added. Additions were made to Config.noSerialNumber.

--- config/config.lua
+++ config/config.lua
@@ -1,24 +1,24 @@
 Lang = "English"
 
 Config = {
-	--======================= DEVELOPMENT ==============================--
-	Debug                    = false, -- if your server is live set this to false.  to true only if you are testing things
+	-- ======================= DEVELOPMENT ============================== --
+	Debug                    = false,   -- If your server is live set this to false.  to true only if you are testing things
 
-	InventoryOrder           = "items", -- items or weapons what should should first in inventory
+	InventoryOrder           = "items", -- Items or weapons what should should first in inventory
 
-	DevMode                  = false, -- if your server is live set this to false.  to true only if you are testing things (auto load inventory when script restart and before character selection. Alos add /getInv command)
+	DevMode                  = false,   -- If your server is live set this to false.  to true only if you are testing things (auto load inventory when script restart and before character selection. Alos add /getInv command)
 	dbupdater                = true,
-	--======================= CONFIGURATION =============================--
-	ShowCharacterNameOnGive  = false, -- when giving an item, show the character name of nearby players instead of their player ID. if set to false, show the player ID
+	-- ======================= CONFIGURATION ============================= --
+	ShowCharacterNameOnGive  = false, -- When giving an item, show the character name of nearby players instead of their player ID. if set to false, show the player ID
 
-	DoubleClickToUse         = true, -- if toggled to false, items in inventory will right click then left click "use"
+	DoubleClickToUse         = true,  -- If toggled to false, items in inventory will right click then left click "use"
 
-	NewPlayers               = false, --- if you dont want new players to give money or items then set to true. this can avoid cheaters giving stuff on first join
+	NewPlayers               = false, -- If you dont want new players to give money or items then set to true. this can avoid cheaters giving stuff on first join
 
-	CoolDownNewPlayer        = 120, -- in seconds how long they have to wait before they can give items or money
+	CoolDownNewPlayer        = 120,   -- In seconds how long they have to wait before they can give items or money
 
 	-- GOLD ITEM LIKE DOLLARS
-	UseRolItem               = false, -- to show rol in inventory
+	UseRolItem               = false,   -- To show rol in inventory
 
 	UseGoldItem              = false,
 
@@ -32,44 +32,45 @@ Config = {
 
 	InventorySearchAutoFocus = true,    -- Search autoofocuses when you type
 
-	DisableDeathInventory    = true,    -- prevent the ability to access inventory while dead
+	DisableDeathInventory    = true,    -- Prevent the ability to access inventory while dead
 
 	OpenKey                  = 0xC1989F95, -- I
-	UseFilter                = true,    -- if true then will use the filter opening inventory
+	UseFilter                = true,       -- If true then will use the filter opening inventory
+	Filter					 = "OJDominoBlur",
 
-	PickupKey                = 0xF84FA74F, --RMB mouse PROMPT PICKUP
+	PickupKey                = 0xF84FA74F, -- RMB mouse PROMPT PICKUP
 
-	discordid                = true,    -- turn to true if ur using discord whitelist
+	discordid                = true,    -- Turn to true if ur using discord whitelist
 
-	DeleteOnlyDontDrop       = false,   -- if true then dropping items only deletes from inventory and box on the floor is not created
+	DeleteOnlyDontDrop       = false,   -- If true then dropping items only deletes from inventory and box on the floor is not created
 
-	UseLanternPutOnBelt      = true,    -- if true then lanterns will be put on belt
+	UseLanternPutOnBelt      = true,    -- If true then lanterns will be put on belt
 
 	WeightMeasure            = "kg",    -- Weight measure (kg, lbs, etc)
 
-	-- =================== CLEAR ITEMS WEAPONS MONEY GOLD =====================--
+	-- =================== CLEAR ITEMS WEAPONS MONEY GOLD ===================== --
 
-	UseClearAll              = false, -- if you want to use the clear item function
+	UseClearAll              = false, -- If you want to use the clear item function
 
 	OnPlayerRespawn          = {
 		Money = {
-			JobLock         = { "police", "doctor" }, -- wont remove from these jobs
-			ClearMoney      = true,          -- if true then removes all money from player
-			MoneyPercentage = false,         -- if false wont use percentage if you add number   0.1 = 10% of money user have instead of all
+			JobLock         = { "police", "doctor" }, -- Wont remove from these jobs
+			ClearMoney      = true,          -- If true then removes all money from player
+			MoneyPercentage = false,         -- If false wont use percentage if you add number   0.1 = 10% of money user have instead of all
 		},
 		Items = {
 			JobLock       = { "police", "doctor" },
-			itemWhiteList = { "consumable_raspberrywater", "ammorevolvernormal" }, -- dont delete these items
-			AllItems      = true,                                         -- if true then removes all items from player
+			itemWhiteList = { "consumable_raspberrywater", "ammorevolvernormal" }, -- Dont delete these items
+			AllItems      = true,                                         -- If true then removes all items from player
 		},
 		Weapons = {
 			JobLock           = { "police", "doctor" },
-			WeaponWhitelisted = { "WEAPON_MELEE_KNIFE", "WEAPON_BOW" }, -- dont delete these weapons
-			AllWeapons        = true,                          -- if true then removes all weapons from player
+			WeaponWhitelisted = { "WEAPON_MELEE_KNIFE", "WEAPON_BOW" }, -- Dont delete these weapons
+			AllWeapons        = true,                          -- If true then removes all weapons from player
 		},
 		Ammo = {
-			JobLock = { "police", "doctor" }, -- wont remove from these jobs
-			AllAmmo = true,          -- if true then removes all ammo from player
+			JobLock = { "police", "doctor" }, -- Wont remove from these jobs
+			AllAmmo = true,          -- If true then removes all ammo from player
 		},
 		Gold = {
 			JobLock        = { "police", "doctor" },
@@ -85,20 +86,20 @@ Config = {
 
 	-- HERE YOU CAN SET THE MAX AMOUNT OF WEAPONS PER JOB (IF YOU WANT)
 	JobsAllowed              = {
-		police = 10 -- job name and max weapons allowed dont allow less than the above
+		police = 10 -- Job name and max weapons allowed dont allow less than the above
 	},
 
 	-- FIRST JOIN
 	startItems               = {
-		consumable_raspberrywater = 2, --ITEMS SAME NAME AS IN DATABASE
-		ammorevolvernormal = 1   --AMMO SAME NAME AS I NTHE DATABASE
+		consumable_raspberrywater = 2, -- ITEMS SAME NAME AS IN DATABASE
+		ammorevolvernormal = 1   -- AMMO SAME NAME AS IN THE DATABASE
 	},
 
 	startWeapons             = {
-		"WEAPON_MELEE_KNIFE" --WEAPON HASH NAME
+		"WEAPON_MELEE_KNIFE" -- WEAPON HASH NAME
 	},
 
-	-- items that dont get added up torwards your max weapon count
+	-- Items that dont get added up torwards your max weapon count
 	notweapons               = {
 		WEAPON_KIT_BINOCULARS_IMPROVED = true,
 		WEAPON_KIT_BINOCULARS = true,
@@ -112,28 +113,41 @@ Config = {
 		WEAPON_MELEE_HAMMER = true,
 		WEAPON_MELEE_KNIFE = true,
 	},
-	-- weapons that are considered non throwables
+
+	-- Weapons that are considered non throwables
 	nonAmmoThrowables        = {
 		WEAPON_MELEE_CLEAVER = true,
 		WEAPON_MELEE_HATCHET = true,
 		WEAPON_MELEE_HATCHET_HUNTER = true
 	},
-	-- weapons that dont need serial numbers
+
+	-- Weapons that dont need serial numbers
 	noSerialNumber           = {
+		WEAPON_MELEE_KNIFE = true,
+		WEAPON_MELEE_KNIFE_JAWBONE = true,
+		WEAPON_MELEE_KNIFE_TRADER = true,
+		WEAPON_MELEE_KNIFE_CIVIL_WAR = true,
+		WEAPON_MELEE_KNIFE_HORROR = true,
+		WEAPON_MELEE_KNIFE_MINER = true,
+		WEAPON_MELEE_KNIFE_RUSTIC = true,
+		WEAPON_MELEE_KNIFE_VAMPIRE = true,
+		WEAPON_MELEE_MACHETE = true,
+		WEAPON_MELEE_MACHETE_COLLECTOR = true,
+		WEAPON_MELEE_HAMMER = true,
+		WEAPON_MELEE_TORCH = true,
 		WEAPON_MELEE_CLEAVER = true,
 		WEAPON_MELEE_HATCHET = true,
 		WEAPON_MELEE_HATCHET_HUNTER = true,
-		WEAPON_MELEE_KNIFE = true,
+		WEAPON_MELEE_HATCHET_DOUBLE_BIT = true,
 		WEAPON_KIT_BINOCULARS_IMPROVED = true,
 		WEAPON_KIT_BINOCULARS = true,
-		WEAPON_FISHINGROD = true,
 		WEAPON_KIT_CAMERA = true,
 		WEAPON_KIT_CAMERA_ADVANCED = true,
+		WEAPON_KIT_METAL_DETECTOR = true,
 		WEAPON_MELEE_LANTERN = true,
 		WEAPON_MELEE_DAVY_LANTERN = true,
 		WEAPON_MELEE_LANTERN_HALLOWEEN = true,
-		WEAPON_KIT_METAL_DETECTOR = true,
-		WEAPON_MELEE_HAMMER = true,
+		WEAPON_FISHINGROD = true,
 		WEAPON_BOW = true,
 		WEAPON_BOW_IMPROVED = true,
 		WEAPON_LASSO = true,
