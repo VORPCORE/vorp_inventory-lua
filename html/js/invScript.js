012087fd0c24279887f4b7883aa176e94e92cad5
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-07-17 00:00:41 +0100

    Update invScript.js

--- html/js/invScript.js
+++ html/js/invScript.js
@@ -387,7 +387,8 @@ function addData(index, item) {
     }
 
     if (item.metadata?.context) {
-        item.metadata.context.forEach(option => {
+        for (let i = 0; i < item.metadata.context.length; i++) {
+            const option = item.metadata.context[i];
             data.push({
                 text: option.text,
                 action: function () {
@@ -396,7 +397,7 @@ function addData(index, item) {
                     );
                 }
             });
-        });
+        }
     }
 
     if (data.length > 0) {
